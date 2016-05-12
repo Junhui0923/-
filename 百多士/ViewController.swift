c44235b3eec32b23f191a8b87c3e9d9f2fe44410
Junhui Li <644633579@qq.com>
2016-05-12 15:35:39 +0200

    Initial Commit

---
+++ 百多士/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  百多士
+//
+//  Created by Junhui on 12/05/2016.
+//  Copyright © 2016 Junhui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
